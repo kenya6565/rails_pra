afa68de1de18d236d3d5c8c7f5e342eb502e882d
EC2 Default User <ec2-user@ip-172-31-36-71.ap-northeast-1.compute.internal>
2020-12-19 14:21:07 +0000

    chapter 5 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'static_pages#home'
+  #rootは/と同義
+  #今回の場合はstatic_pagesが/と同じ
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  #これはstatic_pages/helpでもアクセスできる
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
