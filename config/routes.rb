23de5bc46e80835da192f7446cb5ec4415464ddc
EC2 Default User <ec2-user@ip-172-31-36-71.ap-northeast-1.compute.internal>
2020-12-23 14:36:04 +0000

    haldway through the chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   #get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -6,12 +8,15 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   #rootは/と同義
   #今回の場合はstatic_pagesが/と同じ
-  root 'static_pages#home'
+  
   get  '/help',    to: 'static_pages#help'
   #これはstatic_pages/helpでもアクセスできる
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
