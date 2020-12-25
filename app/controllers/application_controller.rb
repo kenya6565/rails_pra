d4eea505bad2e97753bd8daea8fa977508081b1f
kenya6565 <ken.australia0605@gmail.com>
2020-12-25 15:30:26 +0000

    done for the chapter 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  # ここにセッションヘルパーを仕込むことでどこのコントローラーに対しても使えるようになる
+  include SessionsHelper
   protect_from_forgery with: :exception
   def hello
     render html: "hello, world!"
